05b20090ef37756d3fe546d12bf1ff4c3f0ad95d
agvan <agvandyke25@gmail.com>
2022-07-07 15:10:24 -0500

    confirmed connection to the server

--- server.js
+++ server.js
@@ -7,7 +7,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 
-
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Hello World'
+    });
+});
 
 
 //listener
