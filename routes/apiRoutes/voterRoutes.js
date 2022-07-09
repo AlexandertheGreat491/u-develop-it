51b5eba4430478ceb8df2a524d855b1c469bd655
agvan <agvandyke25@gmail.com>
2022-07-09 13:13:19 -0500

    added route to get all voters & confirmed that endpoint works

--- routes/apiRoutes/voterRoutes.js
+++ routes/apiRoutes/voterRoutes.js
@@ -1,4 +1,21 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const db = require('../../db/connection');
-const inputCheck = require('../../utils/inputCheck');
+const db = require("../../db/connection");
+const inputCheck = require("../../utils/inputCheck");
+
+router.get("/voters", (req, res) => {
+  const sql = `SELECT * FROM voters ORDER BY last_name DESC`;
+
+  db.query(sql, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: "success",
+      data: rows,
+    });
+  });
+});
+
+module.exports = router;
