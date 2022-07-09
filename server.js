944f873f1b9e2ee48bb3aa4d154d8236b322a66c
agvan <agvandyke25@gmail.com>
2022-07-09 11:50:16 -0500

    added require statement for the connection.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const inputCheck = require("./utils/inputCheck");
 const express = require("express");
-
+const db = require('./db/connection');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
